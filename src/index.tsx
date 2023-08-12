15998bb341f9f1ed9001c4d52200cd0d5e8293bd
Long Tran <mail@tranhl.me>
2023-08-12 18:40:53 +1000

    refactor: move stylesheets into `src/styles`

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { CssVarsProvider } from "@mui/joy/styles";
 import CssBaseline from "@mui/joy/CssBaseline";
-import "./index.css";
+import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
